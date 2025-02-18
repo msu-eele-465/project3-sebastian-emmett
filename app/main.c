8c83a94efeab0698fa3f41b3feba86e34b59abe5
Emmett <emmettosborne.01@gmail.com>
2025-02-18 06:46:06 -0700

    Added functionality to interface with the keyboard. First, P6.6 LED toggles whenever any key is pressed. Second, prev_key and curr_key are updated accordingly. Note: This is currently being done in main, Ill move it to an interrupt here in a second. Still not sure how to handle debounce when its on an interrupt at the moment, though.

--- app/main.c
+++ app/main.c
@@ -18,6 +18,9 @@ char prev_key = 0;
 // Function Prototypes
 // ----------------------------------------------------------------------------
 void init_heartbeat(void);    // Heartbeat LED on P1.0 (1 Hz)
+void init_keypad(void);       
+void init_responseLED(void);  // LED on P6.6
+char poll_keypad(void);
 
 // ----------------------------------------------------------------------------
 // MAIN
@@ -28,12 +31,38 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;      // Unlock I/Os
 
     init_heartbeat();    // Set up Timer_B0 for blinking P1.0
+    init_keypad();       // Init keyboard with P6s as input and P5s as output
+    init_responseLED();  // LED on P6.6 to show when a key gets pressed
 
     // Enable global interrupts for the heartbeat timer
     __bis_SR_register(GIE);
 
     while(1)
     {
+        // Poll the keypad
+        char key = poll_keypad();
+
+        if (key != 0)  // A key was detected
+        {
+            // Toggle LED on *any* key press
+            P6OUT ^= BIT6;
+
+            // Shift curr_key -> prev_key, store the new key
+            prev_key = curr_key;
+            curr_key = key;
+
+            // Debounce ~200ms
+            __delay_cycles(200000);
+
+            // Wait until key is released
+            while (poll_keypad() != 0)
+            {
+                // do nothing until no key is pressed
+            }
+        }
+
+        // ~20ms gap between polls
+        __delay_cycles(20000);
     }
 }
 
@@ -60,4 +89,75 @@ void init_heartbeat(void)
 __interrupt void TIMER0_B0_ISR(void)
 {
     P1OUT ^= BIT0;  // Toggle heartbeat LED!
+}
+
+// ----------------------------------------------------------------------------
+// init_keypad: 
+//   Rows (P5.0..3) as outputs
+//   Columns (P6.0..3) as inputs - active-high requires pull-downs
+// ----------------------------------------------------------------------------
+void init_keypad(void)
+{
+    // Rows = outputs, start low
+    P5DIR |= (BIT0 | BIT1 | BIT2 | BIT3);
+    P5OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+
+    // Columns = inputs
+    P6DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+
+    // Enable internal pull-downs
+    P6REN |=  (BIT0 | BIT1 | BIT2 | BIT3);
+    P6OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+}
+
+// ----------------------------------------------------------------------------
+// init_responseLED: LED on P6.6 (off initially)
+// ----------------------------------------------------------------------------
+void init_responseLED(void)
+{
+    P6DIR |= BIT6;
+    P6OUT &= ~BIT6;
+}
+
+// ----------------------------------------------------------------------------
+// poll_keypad:
+//   1) For each row, set that row high, all others low
+//   2) Read columns. If any column bit is 1 => pressed key
+//   3) Return the char from keypadMap[row][col], or 0 if none
+// ----------------------------------------------------------------------------
+char poll_keypad(void)
+{
+    #define ROW_MASK (BIT0 | BIT1 | BIT2 | BIT3)
+
+    int row;
+    for (row = 0; row < 4; row++)
+    {
+        // Clear all rows
+        P5OUT &= ~ROW_MASK;
+        // Drive *only* this row high
+        P5OUT |= (BIT0 << row);
+
+        // Small settle delay - DO NOT REMOVE THIS LMAO
+        __delay_cycles(50);
+
+        // Read columns (low nibble of P6 :D)
+        unsigned char colState = P6IN & 0x0F;
+
+        int col;
+        for (col = 0; col < 4; col++)
+        {
+            // If column bit is high => pressed key
+            if (colState & (1 << col))
+            {
+                // Reset rows
+                P5OUT &= ~ROW_MASK;
+                // Return the key from keypadMap
+                return keypadMap[row][col];
+            }
+        }
+    }
+
+    // No key found
+    P5OUT &= ~ROW_MASK;
+    return 0;
 }
