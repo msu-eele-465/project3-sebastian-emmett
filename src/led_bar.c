50b7da63da7c2c96429fbf4d7e898677c8792c50
Sebastian Carpenter <SebastianRCarpenter@gmail.com>
2025-02-18 22:56:12 -0700

    added patterns and a macro to led_bar.c/.h
    patterns 0 - 7 have been added
    a macro to update the led bar has been added
    
    this has not been tested yet, since CCS kept complaining about a pwm
    dependency that shouldn't exist

--- src/led_bar.c
+++ src/led_bar.c
@@ -10,6 +10,28 @@ extern bool locked;
 extern bool num_update;
 extern bool reset_pattern;
 
+// arrays used for pattern logic
+const char COUNT_3_ARRAY[] = {0x18, 0x24, 0x42, 0x81, 0x42, 0x24};
+#define COUNT_3_ARRAY_MAX_INDEX 5
+
+char pointer_5_array[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
+#define pointer_5_array_END 0x80
+
+char pointer_6_array[] = {0x7F, 0xBF, 0xDF, 0xEF, 0xF7, 0xFB, 0xFD, 0xFE};
+#define pointer_6_array_END 0xFE
+
+char pointer_7_array[] = {0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF};
+#define pointer_7_array_END 0xFF
+
+// pointers and counts used for the pattern logic
+unsigned char count_1 = 0xAA;
+unsigned char count_2 = 0x0;
+unsigned char count_3 = 0;
+unsigned char count_4 = 255;
+char * pointer_5 = pointer_5_array;
+char * pointer_6 = pointer_6_array;
+char * pointer_7 = pointer_7_array;
+
 // ----------------------------------------------------------------------------
 // led_bar_update_pattern:
 //   Single switch case over curr_num, cases for 0-7
@@ -23,7 +45,9 @@ void led_bar_update_pattern(void)
     {
         case '0':
             BTP_multiplier = 1;
-            // Set LED bar to stationary 10101010 #TODO
+
+            led_bar_update(0xAA);   // display 10101010
+
             reset_pattern = false;
             break;
 
@@ -31,84 +55,115 @@ void led_bar_update_pattern(void)
             BTP_multiplier = 4;
             if (reset_pattern)
             {
-                // True outcome space
+                count_1 = 0xAA;                
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                count_1 = ~count_1;
             }
+
+            led_bar_update(count_1);    // display either 10101010 or 01010101
+
             break;
 
         case '2':
             BTP_multiplier = 2;
             if (reset_pattern)
             {
-                // True outcome space
+                count_2 = 0;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                count_2++;              // unsigned expressions cannot overflow, this will automatically
+                                        // roll to 0 at 255
             }
+
+            led_bar_update(count_2);    // display binary count from 0 to 255
+
             break;
 
         case '3':
             BTP_multiplier = 2;
             if (reset_pattern)
             {
-                // True outcome space
+                count_3 = 0;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                (count_3 <= COUNT_3_ARRAY_MAX_INDEX) ? (count_3++) : (count_3 = 0);
+
             }
+
+            led_bar_update(count_3);    // display two led's boucing against each other
+
             break;
 
         case '4':
             BTP_multiplier = 1;
             if (reset_pattern)
             {
-                // True outcome space
+                count_4 = 0;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                count_4--;              // unsigned expressions cannot overflow, this will automatically
+                                        // roll to 255 at 0
             }
+
+            led_bar_update(count_4);    // display binary count from 255 to 0
+
             break;
 
         case '5':
             BTP_multiplier = 6;
             if (reset_pattern)
             {
-                // True outcome space
+                pointer_5 = pointer_5_array;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                (*pointer_5 == pointer_5_array_END) ? (pointer_5 = pointer_5_array) : (pointer_5++);
             }
+
+            led_bar_update(*pointer_5); // display a single bit shifting across
+
             break;
 
         case '6':
             BTP_multiplier = 2;
             if (reset_pattern)
             {
-                // True outcome space
+                pointer_6 = pointer_6_array;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                (*pointer_6 == pointer_6_array_END) ? (pointer_6 = pointer_6_array) : (pointer_6++);
             }
+
+            led_bar_update(*pointer_6); // display an unflipped bit shifting across
+
             break;
 
         case '7':
             BTP_multiplier = 4;
             if (reset_pattern)
             {
-                // True outcome space
+                pointer_7 = pointer_7_array;
+                reset_pattern = false;
             }
             else
             {
-                // False outcome space
+                (*pointer_7 == pointer_7_array_END) ? (pointer_7 = pointer_7_array) : (pointer_7++);
             }
+
+            led_bar_update(*pointer_7); // display an every growing number of set bits
+
             break;
 
         default:
--- src/led_bar.h
+++ src/led_bar.h
@@ -2,6 +2,11 @@
 #define LED_BAR_H
 
 void led_bar_update_pattern(void);
+
 void led_bar_delay(void);
+
+#define led_bar_update(update_value)\
+    P3OUT = 0;\
+    P3OUT |= update_value
 
 #endif
