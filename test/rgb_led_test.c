4cba2e647071631b4be101c3229c390d229d2ee2
Sebastian Carpenter <SebastianRCarpenter@gmail.com>
2025-02-19 10:46:50 -0700

    rgb_led is mostly working now
    colors are not perfect but are 'fairly' close

--- test/rgb_led_test.c
+++ test/rgb_led_test.c
@@ -7,12 +7,43 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog
 
     pwm_init();
+    unsigned short i = 0;
+    unsigned short j = 0;
 
     PM5CTL0 &= ~LOCKLPM5;      // Unlock I/Os
 
-    //while(1);
+    while(1)
+    {
+        // purple
+        rgb_set(0xb7, 0x0, 0xff);
 
-    rgb_set(0xc8, 0x00, 0xff);
+        for(j = 0; j < 5; j++)
+        {
+            for(i = 0; i < 65000; i++);
+        }
 
-    while(1);
+        // orange
+        rgb_set(0xff, 0x91, 0x00);
+
+        for(j = 0; j < 5; j++)
+        {
+            for(i = 0; i < 65000; i++);
+        }
+
+        // cyan
+        rgb_set(0x00, 0xff, 0xee);
+
+        for(j = 0; j < 5; j++)
+        {
+            for(i = 0; i < 65000; i++);
+        }
+
+        // yellow
+        rgb_set(0xFB, 0xFF, 0x00);
+
+        for(j = 0; j < 5; j++)
+        {
+            for(i = 0; i < 65000; i++);
+        }
+    }
 }
