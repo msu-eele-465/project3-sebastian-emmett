f175f719b880a4451670787b19eab973ad1124e5
EmmettOsborne <EmmettOsborne.01@gmail.com>
2025-02-20 11:10:17 -0700

    Fixed some bugs (warnings), increased brightness, added final planning docs

--- src/rgb_led.c
+++ src/rgb_led.c
@@ -5,7 +5,7 @@ void rgb_set(unsigned char red_intensity, unsigned char green_intensity, unsigne
 {
     // Since certain colors use more current or eyes can be more sensitive
     // the pwm ratios are tuned slightly through multiplication
-    pwm_set_duty_ccr1((unsigned short)red_intensity * 9);
-    pwm_set_duty_ccr2((unsigned short)green_intensity * 4.5);
-    pwm_set_duty_ccr3((unsigned short)blue_intensity * 6);
+    pwm_set_duty_ccr1((unsigned short)red_intensity * 12);
+    pwm_set_duty_ccr2((unsigned short)green_intensity * 6);
+    pwm_set_duty_ccr3((unsigned short)blue_intensity * 8);
 }
