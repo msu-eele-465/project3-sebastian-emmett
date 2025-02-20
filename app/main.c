d526e9724b142f58d6d883d5425911e608015758
Sebastian Carpenter <SebastianRCarpenter@gmail.com>
2025-02-19 19:49:04 -0700

    added basic led states to system
    locked, unlocking, and unlocked are now present

--- app/main.c
+++ app/main.c
@@ -1,9 +1,11 @@
 #include <msp430.h>
 #include <stdbool.h>
 #include <string.h>
-#include "../src/keyboard.h"  // Include this to use init_keypad() and poll_keypad()
-#include "../src/heartbeat.h" // For init_heartbeat()
-#include "../src/led_bar.h" // For led_bar_update_pattern() and led_bar_delay
+#include "../src/keyboard.h"    // Include this to use init_keypad() and poll_keypad()
+#include "../src/heartbeat.h"   // For init_heartbeat()
+#include "../src/led_bar.h"     // For led_bar_update_pattern() and led_bar_delay
+#include "../src/rgb_led.h"     // For adjusting the pwm signal meant for rgb_led
+#include "../src/pwm.h"         // For creating the pwm signal on P6.0 - P6.2
 
 // ----------------------------------------------------------------------------
 // Globals! (yes they deserve their own lil space)
@@ -35,33 +37,32 @@ int BTP_multiplier = 0;
 // Variables for our passcode
 bool unlocking = false;             // True while we're collecting 4 digits
 static const char correct_pass[] = "1234";  // Hard-coded correct passcode
-char pass_entered[5];              // Room for 4 digits + null terminator
-unsigned pass_index = 0;           // How many digits we've collected so far
+char pass_entered[5];               // Room for 4 digits + null terminator
+unsigned pass_index = 0;            // How many digits we've collected so far
 
 // Timeout tracking: We'll use the heartbeat (1Hz) to decrement
-volatile int pass_timer = 0;       // 5-second countdown if unlocking
-
-// ----------------------------------------------------------------------------
-// Function Prototypes
-// ----------------------------------------------------------------------------
-void init_heartbeat(void);  // Heartbeat LED on P1.0 (1 Hz)
+volatile int pass_timer = 0;        // 5-second countdown if unlocking
 
 // ----------------------------------------------------------------------------
 // MAIN
 // ----------------------------------------------------------------------------
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog
-    PM5CTL0 &= ~LOCKLPM5;      // Unlock I/Os
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog
 
-    init_heartbeat();    // Set up Timer_B0 for blinking P1.0
-    init_keypad();       // Init keyboard with P6s as input and P5s as output
-    init_responseLED();  // LED on P6.6 to show when a key gets pressed
-    init_keyscan_timer(); // Timer_B1 => ~50 ms interrupt
+    init_heartbeat();           // Set up Timer_B0 for blinking P1.0
+    init_keypad();              // Init keyboard with P6s as input and P5s as output
+    init_responseLED();         // LED on P6.6 to show when a key gets pressed
+    init_keyscan_timer();       // Timer_B1 => ~50 ms interrupt
+    led_bar_init();             // configure P3.0 - P3.7 as outputs for led bar
+    pwm_init();                 // start pwm signal on P6.0 - P6.2
+
+    PM5CTL0 &= ~LOCKLPM5;       // Unlock I/Os
 
     // Enable global interrupts for the heartbeat timer
     __bis_SR_register(GIE);
 
+    rgb_set(0xC4, 0x3E, 0x1D);  // start state led as red color, for locked state
     while(1)
     {
         // ----------------------------------------------------------------------------
@@ -80,7 +81,8 @@ int main(void)
             {
                 // We got a numeric press => start unlocking process
                 unlocking = true;
-                num_update = false;  // We consumed this press
+                num_update = false;     // We consumed this press
+                rgb_set(0xC4, 0x92, 0x1D);      // set state led to yellowish color, for unlocking
 
                 // Start collecting passcode digits
                 pass_index = 0;
@@ -104,6 +106,8 @@ int main(void)
             {
                 // Time’s up => reset D:
                 unlocking = false;
+
+                rgb_set(0xC4, 0x3E, 0x1D);  // set state led as red color, for locked state
             }
             else
             {
@@ -118,12 +122,15 @@ int main(void)
                 }
                 else
                 {
+                    rgb_set(0xC4, 0x3E, 0x1D);      // set state LED to red color, for locked
+
                     // 4 digits => compare
                     pass_entered[4] = '\0';
                     if (strcmp(pass_entered, correct_pass) == 0)
                     {
                         // Correct => unlock
                         locked = false;
+                        rgb_set(0x1D, 0xA2, 0xC4);  // set state LED to blueish color, for unlocked
                     }
                     // Otherwise => stay locked, reset
                     unlocking = false;
@@ -138,6 +145,8 @@ int main(void)
         {
             // locked == false => update led_bar
             led_bar_update_pattern();
+            locked = false;
+            num_update = false;
             led_bar_delay();
         }
     }
